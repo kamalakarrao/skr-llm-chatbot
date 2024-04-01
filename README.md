2e706b294acaccd77286b861f471e2db63b11c66
S.Kamalakar <kamalakar.sambaraj@gmail.com>
2024-04-01 15:40:38 +0000

    Test Commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 📦 Streamlit App Starter Kit 
 ```
-⬆️ (Replace above with your app's name)
+LLM Chatbot
 ```
 
 Description of the app ...
--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ st.title('🤖 ML Model Building')
 
 with st.expander('About this app'):
   st.markdown('**What can this app do?**')
-  st.info('This app allow users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
+  st.info('This app allow the users to build a machine learning (ML) model in an end-to-end workflow. Particularly, this encompasses data upload, data pre-processing, ML model building and post-model analysis.')
 
   st.markdown('**How to use the app?**')
   st.warning('To engage with the app, go to the sidebar and 1. Select a data set and 2. Adjust the model parameters by adjusting the various slider widgets. As a result, this would initiate the ML model building process, display the model results as well as allowing users to download the generated models and accompanying data.')
